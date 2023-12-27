f7ed6f74acd48f8eabe9381186cc45657e6efa95
sebanimm <gks32114221@gmail.com>
2023-12-27 20:03:55 +0900

    라우터 적용

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
