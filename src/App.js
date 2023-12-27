33875c439806d9c5ad5e1dd32000738ae7b13af4
sebanimm <gks32114221@gmail.com>
2023-12-28 02:18:38 +0900

    피브 추가 (기능구현X)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "../src/pages/Login";
+import Feed from "../src/pages/Feed";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/feed" element={<Feed />} />
         <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
---
+++ src/pages/Feed.jsx
@@ -0,0 +1,176 @@
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import Header from "../components/Header";
+import decreaseIcon from "../assets/decreaseIcon.svg";
+import increaseIcon from "../assets/increaseIcon.svg";
+
+const currentYear = new Date().getFullYear();
+const currentMonth = new Date().getMonth() + 1;
+const currentDay = new Date().getDate();
+
+const vw = Math.max(
+  document.documentElement.clientWidth || 0,
+  window.innerWidth || 0,
+);
+
+const Feed = () => {
+  const [scroll, setScroll] = useState(0);
+  const [year, setYear] = useState(currentYear);
+  const [month, setMonth] = useState(currentMonth);
+  const [day, setDay] = useState(currentDay);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollHeight = window.scrollY;
+      setScroll(
+        Math.round((currentScrollHeight / maxHeight) * 100) / 100 + 0.01,
+      );
+    };
+
+    const scrollHeight = Math.max(
+      document.body.scrollHeight,
+      document.documentElement.scrollHeight,
+      document.body.offsetHeight,
+      document.documentElement.offsetHeight,
+      document.body.clientHeight,
+      document.documentElement.clientHeight,
+    );
+
+    const maxHeight = scrollHeight - window.innerHeight;
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  });
+
+  const increaseYear = () => {
+    setYear((prev) => prev + 1);
+  };
+
+  const decreaseYear = () => {
+    setYear((prev) => prev - 1);
+  };
+
+  const increaseMonth = () => {
+    setMonth((prev) => prev + 1);
+    if (month >= 12) {
+      increaseYear();
+      setMonth(1);
+    }
+  };
+
+  const decreaseMonth = () => {
+    setMonth((prev) => prev - 1);
+    if (month === 1) {
+      decreaseYear();
+      setMonth(12);
+    }
+  };
+
+  const increaseDay = () => {
+    setDay((prev) => prev + 1);
+    const firstDay = new Date(year, month, 1).getDate();
+    const lastDay = new Date(year, month + 1, 0).getDate();
+    if (day === lastDay) {
+      increaseMonth();
+      setDay(firstDay);
+    }
+  };
+
+  const decreaseDay = () => {
+    setDay((prev) => prev - 1);
+    const firstDay = new Date(year, month, 1).getDate();
+    const lastDay = new Date(year, month + 1, 0).getDate();
+    if (day === firstDay) {
+      decreaseMonth();
+      setDay(lastDay);
+    }
+  };
+
+  return (
+    <Container>
+      <Header />
+      <ScrollBarBackground>
+        <ScrollBar $scrollHeight={scroll} $vw={vw} />
+      </ScrollBarBackground>
+      <Background>
+        <Dates>
+          <IncreaseButton onClick={increaseYear}>
+            <img src={increaseIcon} alt="increase" />
+          </IncreaseButton>
+          <span style={{ marginBottom: "28px" }}>{year}</span>
+          <DecreaseButton onClick={decreaseYear}>
+            <img src={decreaseIcon} alt="decrease" />
+          </DecreaseButton>
+        </Dates>
+        <Dates>
+          <IncreaseButton onClick={increaseDay}>
+            <img src={increaseIcon} alt="increase" />
+          </IncreaseButton>
+          <span style={{ marginBottom: "28px" }}>
+            {String(month).padStart(2, "0")}
+            {String(day).padStart(2, "0")}
+          </span>
+          <DecreaseButton onClick={decreaseDay}>
+            <img src={decreaseIcon} alt="decrease" />
+          </DecreaseButton>
+        </Dates>
+      </Background>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  min-height: 100vh;
+  background: #ffffff;
+`;
+
+const ScrollBarBackground = styled.div`
+  background: #dedede;
+  width: 100%;
+  height: 8px;
+  position: fixed;
+  top: 72px;
+`;
+
+const ScrollBar = styled.div`
+  height: 8px;
+  position: relative;
+  background: #ff8c4b;
+  width: ${({ $scrollHeight, $vw }) => $scrollHeight * $vw}px;
+  border-radius: 4px;
+`;
+
+const Background = styled.div`
+  position: fixed;
+  top: calc(30vh);
+  font-size: 200px;
+  color: #f1f1f1;
+  font-weight: 900;
+  width: 90%;
+  display: flex;
+  justify-content: space-between;
+  padding-inline: 5%;
+  user-select: none;
+`;
+
+const DecreaseButton = styled.div`
+  padding: 12px;
+  border-radius: 40px;
+  background: #f0f0f0;
+  display: flex;
+  cursor: pointer;
+  width: fit-content;
+`;
+
+const IncreaseButton = styled(DecreaseButton)``;
+
+const Dates = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export default Feed;
